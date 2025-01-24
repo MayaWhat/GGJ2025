cc3b5e8ec124e104c1bba69ad0be399fb26b80b6
MayaWhat <ian@devolutious.co.nz>
2025-01-25 10:39:20 +1300

    Popped music

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -2,20 +2,14 @@ using Godot;
 
 public partial class MusicPlayer : AudioStreamPlayer
 {
-    AudioStreamPlaybackInteractive playback;
-
-    public override void _Ready()
-    {
-        base._Ready();
-        playback = Stream.InstantiatePlayback() as AudioStreamPlaybackInteractive;
-    }
+    AudioStreamPlaybackInteractive GetInteractiveStreamPlayback() => GetStreamPlayback() as AudioStreamPlaybackInteractive;
 
     public void PlayClip(string clip)
     {
-        playback.SwitchToClipByName(clip);
         if (!Playing)
         {
             Play();
         }
+        GetInteractiveStreamPlayback().SwitchToClipByName(clip);
     }
 }
